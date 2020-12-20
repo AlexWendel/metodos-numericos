2c1033d4912f1f9e4c2c4a844d42df8b961e0d67
Yures1lva <yure.siilva9@gmail.com>
2020-12-20 16:38:26 -0300

    corrigindo simpson

--- regra_de_simpson.py
+++ regra_de_simpson.py
@@ -1,8 +1,4 @@
 
-a = 0
-b = 0,8
-n = 5
-passo = (b - a)/n
 
 
 def funcx(x):
@@ -10,10 +6,7 @@ def funcx(x):
     return fx
 
 
-# subintervalos = []
 
-# for i in range(0,n):
-#    subintervalos = [passo*2]
 
 def calc_umterco(h,a,ab,b):#utiliza polinomios de 2 graus, 
                         #so vale para numeros pares no intervalo
@@ -23,3 +16,21 @@ def calc_umterco(h,a,ab,b):#utiliza polinomios de 2 graus,
 def calc_tresoitavos(h,a,x2,x3,b):#utiliza polinomios de 3 grau
       #so vale para numeros de intervalos divisiveis por 3
     return (3*h/8)*(funcx(a) + 3*(x2) + 3*funcx(x3) + funcx(b))
+
+
+
+
+
+
+a = input("Defina o começo do intervalo inicial: ")
+b = input("Defina o fim do intervalo inicial: ")
+print("intervalo inicial definido como [",a,";",b,"]")
+n = input("defina o numero de seguimentos n: ")
+
+passo = (b - a)/n
+
+# subintervalos = []
+# subintervalos[0] = 0
+# subintervalos[1] = passo
+# for i in range(2,n):
+#    subintervalos[i] = subintervalos[1]*i
