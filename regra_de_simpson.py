a6d51215d87149ea7da07307c9337ecb7f12fb38
Yures1lva <yure.siilva9@gmail.com>
2020-12-20 15:59:42 -0300

    implementando regras 1/3 e 3/8

--- regra_de_simpson.py
+++ regra_de_simpson.py
@@ -5,15 +5,21 @@ n = 5
 passo = (b - a)/n
 
 
+def funcx(x):
+    fx = (0.2 + 25*x -(200*x)**2 -(675*x)**3 -(900*x)**4 + (400*x)**5)     
+    return fx
 
-subintervalos = []
-for i in range(0,n):
-   subintervalos = [passo*2]
 
-def calc_umterco():#utiliza polinomios de 2 graus, 
-                   #so vale para numeros pares no intervalo
-    print("porras")
+# subintervalos = []
 
-def calc_tresoitavos():#utiliza polinomios de 3 grau
-    print("porras")    #so vale para numeros de intervalos divisiveis por 3
-    
+# for i in range(0,n):
+#    subintervalos = [passo*2]
+
+def calc_umterco(h,a,ab,b):#utiliza polinomios de 2 graus, 
+                        #so vale para numeros pares no intervalo
+    return (h/3)*(funcx(a) + 4*(funcx(ab)) + funcx(b))
+
+
+def calc_tresoitavos(h,a,x2,x3,b):#utiliza polinomios de 3 grau
+      #so vale para numeros de intervalos divisiveis por 3
+    return (3*h/8)*(funcx(a) + 3*(x2) + 3*funcx(x3) + funcx(b))
