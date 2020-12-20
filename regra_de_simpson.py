c07d2e8caba6b56aa41dd61d8367444689b64b18
Yures1lva <yure.siilva9@gmail.com>
2020-12-20 11:05:13 -0300

    começamdo regra de simpson

---
+++ regra_de_simpson.py
@@ -0,0 +1,19 @@
+
+a = 0
+b = 0,8
+n = 5
+passo = (b - a)/n
+
+
+
+subintervalos = []
+for i in range(0,n):
+   subintervalos = [passo*2]
+
+def calc_umterco():#utiliza polinomios de 2 graus, 
+                   #so vale para numeros pares no intervalo
+    print("porras")
+
+def calc_tresoitavos():#utiliza polinomios de 3 grau
+    print("porras")    #so vale para numeros de intervalos divisiveis por 3
+    
