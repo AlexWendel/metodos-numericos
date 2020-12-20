91e0e9e879a8756b4cdcddb58e99e06459f152c6
Gust4Oliveira <gustavo.larcedabh2@gmail.com>
2020-12-20 00:42:10 -0300

    Adição e tradução de comentários

--- gauss.py
+++ gauss.py
@@ -1,36 +1,34 @@
-
 #Dados da matriz
 A = input("Digite a matriz: Ex:[[1,2,3],[4,5,6]]\n")
-A = eval(A)
-linhas = len(A)
-colunas = len(A[0])
-rounding = 6
+A = eval(A) #Traduz a string com a matriz para uma matriz regular em python
+linhas = len(A) #Captura a quantidade de linhas da matriz
+colunas = len(A[0]) #Captura a quantidade de colunas da matriz usando a primeira linha
+rounding = 6 #Quantiade de casas decimais usadas para o arredondamento
 
-def printmatrix(matrix):
+def printmatrix(matrix): #Printa a matriz
     for i, item in enumerate(matrix):
         print(f"A[{i}]=", item)
 
 
-def getMultiplierCoeficient(column, endline, matrix):
+def getMultiplierCoeficient(column, endline, matrix): #Método para obter os multiplicadores a partir do pivo
     pivot = matrix[column][column]
     multipliers = []
     for i in range(0, endline):
         if i < column:
-            multipliers.append(0)
+            multipliers.append(0) #Gera um multiplicador 0 para as colunas que ja foram zeradas
         else:
             try:
-                multipliers.append(round(A[i][column]/pivot, rounding))
+                multipliers.append(A[i][column]/pivot)
             except ZeroDivisionError:
                 multipliers.append(0)
     return multipliers
 
 
-def applyElimination(startline, endline, endcolumn, matrix, multipliers):
+def applyElimination(startline, endline, endcolumn, matrix, multipliers): #Método para aplicar os multiplicadores obtidos na matriz
     for i in range(startline+1, endline):
         for j in range(startline, endcolumn):
             A[i][j] = round((multipliers[i]*A[startline][j] - A[i][j]), rounding)
     return A
-
 
 print("Matriz inicial:")
 printmatrix(A)
@@ -40,7 +38,7 @@ for i in range(0, linhas):
     A = applyElimination(i, linhas, colunas, A, multiplier)
 print("Matriz final:")
 
-for j in range(0,linhas): #Matrix rearrangement if needed 
+for j in range(0,linhas): #Reordenação da matriz se necessário
     for k in range(0,colunas):
         if j > k:
             if A[j][k] != 0:
@@ -54,14 +52,14 @@ solution = []
 for i in range(0, linhas-1):
     solution.append(0)
 solution.append(round(A[linhas-1][colunas-1]/A[linhas-1]
-                      [colunas-2], rounding))  # Last line
+                      [colunas-2], rounding))  #Último coeficiente dependente desconhecido sempre é a sua divisão com o termo independente da linha
 
-for i in range(linhas-2, -1, -1):  # Last to first line
+for i in range(linhas-2, -1, -1):  #Loop para obter os coeficientes depententes da penultima linha para a primeira
     sum = 0
-    for j in range(colunas-2, -1, -1):  # Last to first column of dependent terms
+    for j in range(colunas-2, -1, -1):
         if A[i][j] != 0 and A[i][j-1] != 0:
             sum = A[i][j] * solution[j] + sum
 
-    solution[i] = round((-sum + A[i][colunas-1])/A[i][i],rounding) #Divide the sum of know dependent terms and the independent term by the unknown dependent term of the line
+    solution[i] = round((-sum + A[i][colunas-1])/A[i][i],rounding) #Divide a soma dos termos dependentes conhecidos e o termo independente pelo termo depentende desconhecido
 
 print(f"Vetor solução {solution}T")
