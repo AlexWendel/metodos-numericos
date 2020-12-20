ce954757237aca3c85503b03d16b2aecf39242dc
Alex Wendel <43787536+AlexWendel@users.noreply.github.com>
2020-12-20 16:34:53 -0300

    organizando bagunça
    YURO MACACO NÃO FAZ O BAGUI DIREITO

--- regra_de_simpson.py
+++ regra_de_simpson.py
@@ -1,25 +1,22 @@
+def calc_func(x):
+    return (0.2 + 25*x -(200*x)**2 -(675*x)**3 -(900*x)**4 + (400*x)**5)
 
-a = 0
-b = 0,8
+# Utiliza polinômios de 2 graus
+# Só vale para números pares no intervalo
+def calc_umterco(h, a, ab, b):
+    a = funcx(a)
+    ab = funcx(ab)
+    b = funcx(b)
+    return (h/3) * (a + 4 * ab + b)
+
+# Utiliza polinomios de 3 grau
+# Só vale para números de intervalos divisíveis por 3
+def calc_tresoitavos(h, a, x2, x3, b):      
+    a = funcx(a)
+    b = funcx(b)
+    return (3*h/8) * (a + 3*func(x2) + 3*funcx(x3) + b)
+
+dados = []
+a, b = 0, 0.8
 n = 5
-passo = (b - a)/n
-
-
-def funcx(x):
-    fx = (0.2 + 25*x -(200*x)**2 -(675*x)**3 -(900*x)**4 + (400*x)**5)     
-    return fx
-
-
-# subintervalos = []
-
-# for i in range(0,n):
-#    subintervalos = [passo*2]
-
-def calc_umterco(h,a,ab,b):#utiliza polinomios de 2 graus, 
-                        #so vale para numeros pares no intervalo
-    return (h/3)*(funcx(a) + 4*(funcx(ab)) + funcx(b))
-
-
-def calc_tresoitavos(h,a,x2,x3,b):#utiliza polinomios de 3 grau
-      #so vale para numeros de intervalos divisiveis por 3
-    return (3*h/8)*(funcx(a) + 3*(x2) + 3*funcx(x3) + funcx(b))
+h=(b-a)/n
