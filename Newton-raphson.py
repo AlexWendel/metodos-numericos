e0e041c9dba4c0ca0c6259cb71c154dfbb7f0270
Alex Wendel <alexwendel11@gmail.com>
2020-12-21 20:26:59 -0300

    Equações de x e y

--- Newton-raphson.py
+++ Newton-raphson.py
@@ -1,4 +1,15 @@
-#sai essa madru ainda
+import math
+import sympy
+
+def calc_x(x, u, v, dv_y, dv_x, du_y, du_x):
+    dividendo = ((u * dv_y) - (v * du_y))
+    divisor = ((du_x * dv_y) - (du_y * dv_x))
+    return x - (dividendo/divisor)
+
+def calc_y(y, u, v, dv_y, dv_x, du_y, du_x):
+    dividendo = ((v * du_x) - (u * dv_x))
+    divisor = ((du_x * dv_y) - (du_y * dv_x))
+    return y - (dividendo/divisor)
 
 def u(x,y):
     return (x**2) + x*y - 10
@@ -6,12 +17,5 @@ def u(x,y):
 def v(x,y):
     return y + 3*x*(y**2) - 57
 
-print("digite o par correto")
-
 x = float(input("x: "))
-y = float(input("y: "))   
-print()
-
-print("par correto definido como: ",x,",",y)
-print()
-
+y = float(input("y: "))
