a89c7e55439e5c5484824fe1e7ee8a1ba5afdfe6
Yures1lva <yure.siilva9@gmail.com>
2020-12-21 18:48:16 -0300

    adicionando simpson composta

--- regra_de_simpson.py
+++ regra_de_simpson.py
@@ -23,18 +23,51 @@ def calc_tresoitavos(h, a, x2, x3, b):
 a = float(input("Defina o começo do intervalo inicial: "))
 b = float(input("Defina o fim do intervalo inicial: "))
 print("intervalo inicial definido como [",a,";",b,"]")
-n = float(input("defina o numero de seguimentos n: "))
+n = int(input("defina o numero inteiro de seguimentos n: "))
 
-h = (b - a)/n
+h = (b - a)/n #passo
+print("passo utilizado: ", h)
 
-subint = [0]*5
-print(subint)
+subint = [0]*(n+1) #vetor de subintervalos
 
-for i in range(1,len(subint)):
+
+#preenchendo o vetor com os subintervalos
+for i in range(len(subint)):
     subint[i] = h*i    
 
+resto1 = (n) % 2
+resto2 = (n) % 3
+result = 0
 
-print(subint)
+print(" intervalos utilizados: ",subint)
+#verificando se o numero de subintervalos é par
+if resto1 == 0 :
+    print("o numero de subintervalos é par, será resolvida com simpson 1/3")
+    
+    
+    # if n == 2:
+    #     result = calc_umterco(h ,subint[0],subint[1],subint[2])
+    #     print("O intervalo total é: ",result)
+    # elif n == 3:
+    #     result = calc_umterco(h ,subint[0],subint[1],subint[2]) + calc_umterco(h ,subint[2],subint[3],subint[4])
+    #     print("O intervalo total é: ",result)
+    # elif n == 7:
+    #     result = calc_umterco(h ,subint[0],subint[1],subint[2]) + calc_umterco(h ,subint[2],subint[3],subint[4]) + calc_umterco(h ,subint[4],subint[5],subint[6]) 
+    #     print("O intervalo total é: ",result)
+    
+elif resto2 == 0:
+    print("o numero de subintervalos é divisivel por 3, será resolvida com simpson 3/8")
+  
+
+else :
+    print(" o numero de subintervalos não é par e nem divisivel por 3,será resolvida com simpson composta")
+   
+    result = calc_umterco(h ,subint[0],subint[1],subint[2]) + calc_tresoitavos(h, subint[2], subint[3],subint[4],subint[5])
+
+    print("O intervalo total é: ",result)
+
+
+
 
 # subintervalos = {
 #     0 : h,
