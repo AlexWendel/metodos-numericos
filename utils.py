fee792369298851e312178f58d92e0909864088c
Alex Wendel <alexwendel11@gmail.com>
2020-12-21 18:43:31 -0300

    adicionando funções úteis

--- utils.py
+++ utils.py
@@ -1,5 +1,27 @@
+import math
+
+pi = math.pi
+
 # CUIDADO QUE ESSA PORRA PODE TROCAR OS VALORES, DEPENDENDO DA ORDEM DOS ITENS DO DICIONÁRIO
 def get_partial_eq(eq, values):
     for k,v in values.items():
         eq = eq.replace(str(k), str(v))
     return eq
+
+def ln(x):
+    return math.log(x, 10)
+
+def sen(x):
+    return math.sin(x)
+
+def tg(x):
+    return math.tan(x)
+
+def cos(x):
+    return math.cos(x)
+
+def arcos(x):
+    return math.acos()
+
+def arcsen(x):
+    return math.asin()
