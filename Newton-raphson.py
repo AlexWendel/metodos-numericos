c8a5d81ef08cb2316bd79838b26a58bc034bbcfc
Yures1lva <yure.siilva9@gmail.com>
2020-12-21 20:14:15 -0300

    começando Newton-Raphson

--- Newton-raphson.py
+++ Newton-raphson.py
@@ -1,1 +1,17 @@
 #sai essa madru ainda
+
+def u(x,y):
+    return (x**2) + x*y - 10
+
+def v(x,y):
+    return y + 3*x*(y**2) - 57
+
+print("digite o par correto")
+
+x = float(input("x: "))
+y = float(input("y: "))   
+print()
+
+print("par correto definido como: ",x,",",y)
+print()
+
