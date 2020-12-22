df8239e025e6d7f28c9403e2e6df0ce73bbd4979
Alex Wendel <alexwendel11@gmail.com>
2020-12-22 15:18:55 -0300

    organização e entrada de dados

--- lagrange.py
+++ lagrange.py
@@ -6,24 +6,25 @@ from utils import get_partial_eq
 fn_eq = "li * y_i"
 li_eq = "(x - x_j) / (x_i - x_j)"
 
+points = int(input("Quantidade de pontos: "))
+degree = int(input("Digite o grau do polinômio: "))
+x = float(input("Digite o valor do ponto: "))
+
+table_matrix = [[0,0] for i in range(points)]
+for i in range(points):
+    table_matrix[i][0] = float(input("x" + str(i) +" = "))
+    table_matrix[i][1] = float(input("y" + str(i) +" = "))
+
 def calc_li(xi, xj, x="x"):
     return get_partial_eq(li_eq, {'x_j': xj, 'x_i': xi, 'x': x})
 
-# TODO: Simplificar e dividir o cálculo da equação em funções menores
-def calc_fn():
-    return 
-
-# TODO: Implementar checagem de erro real
-def calc_error(real, result):
-    return abs((real-result)/real)
-
-table_matrix = [
-    [1,0],
-    [4,1.386294],
-    [6,1.791760]
-] # Tabela de valores
-grau_target = 2 # Grau da equação
-table_rows = table_matrix[:grau_target+1] # Hardcoded, não mexa
+# table_matrix = [
+#     [1,0],
+#     [4, 1.386294],
+#     [6, 1.791760]
+# ] # Tabela de valores
+# degree = 2 # Grau da equação
+table_rows = table_matrix[:degree+1] # Hardcoded, não mexa
 rows_length = len(table_rows) # Quantidade de rows a serem analisadas (NÃO MEXA)
 rounding = 6 # Quantidade de casas depois da vírgula
 
@@ -43,7 +44,7 @@ for i in range(rows_length):
     iterations.append(final_iteration_li)
 
 glued_equation = " + ".join(iterations)
-partial_equation = get_partial_eq(glued_equation, {'x': 2})
-print(partial_equation)
+partial_equation = get_partial_eq(glued_equation, {'x': x})
+print(f"f({x}) = {partial_equation}")
 result = eval(partial_equation)
-print(round(result, rounding))
+print(f"Resultado: {round(result, rounding)}")
