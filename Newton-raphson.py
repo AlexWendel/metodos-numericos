55f44afad3af8c7cda781b1f3d2ca76b5e82edbe
Yures1lva <yure.siilva9@gmail.com>
2020-12-21 23:22:45 -0300

    adicionando derivarada no cu do Newton

--- Newton-raphson.py
+++ Newton-raphson.py
@@ -1,15 +1,8 @@
 import math
-import sympy
+from sympy import *
 
-def calc_x(x, u, v, dv_y, dv_x, du_y, du_x):
-    dividendo = ((u * dv_y) - (v * du_y))
-    divisor = ((du_x * dv_y) - (du_y * dv_x))
-    return x - (dividendo/divisor)
-
-def calc_y(y, u, v, dv_y, dv_x, du_y, du_x):
-    dividendo = ((v * du_x) - (u * dv_x))
-    divisor = ((du_x * dv_y) - (du_y * dv_x))
-    return y - (dividendo/divisor)
+x,y = symbols('x y')
+init_printing(use_unicode=True)
 
 def u(x,y):
     return (x**2) + x*y - 10
@@ -17,5 +10,37 @@ def u(x,y):
 def v(x,y):
     return y + 3*x*(y**2) - 57
 
-x = float(input("x: "))
-y = float(input("y: "))
+print("Digite o par x y correto")
+a = float(input("x: "))
+b = float(input("y: "))
+print()
+print("U0 = ", u(a,b))
+print("v0 = ",v(a,b))
+
+
+
+def du_x(x,y):
+    return diff(u(x,y), x)
+
+def du_y(x,y):
+    return diff(u(x,y), y)
+
+def dv_x(x,y):
+    return diff(v(x,y), x)
+
+def dv_y(x,y):
+    return diff(v(x,y), y) 
+
+
+
+divisor = ((du_x(x,y) * dv_y(x,y)) - (du_y(x,y) * dv_x(x,y)))
+
+def calc_x(x, u, v, dv_y, dv_x, du_y, du_x):
+    dividendo = ((u * dv_y) - (v * du_y))
+    
+    return x - (dividendo/divisor)
+
+def calc_y(y, u, v, dv_y, dv_x, du_y, du_x):
+    dividendo = ((v * du_x) - (u * dv_x))
+    # divisor = ((du_x * dv_y) - (du_y * dv_x))
+    return y - (dividendo/divisor)
